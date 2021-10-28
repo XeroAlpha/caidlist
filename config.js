15844446856abddf777880967c1416e5554b5006
ProjectXero <projectxero@163.com>
2021-10-28 21:43:23 +0800

    feat(version): 更新至Beta 1.18.0.24

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 安装包版本
-exports.installPackageVersion = "1.17.41.01";
+exports.installPackageVersion = "1.18.0.24";
 // 安装包路径
-exports.installPackagePath = "H:\\BedrockVersions\\New\\Minecraft_1.17.41 (1.17.41.01)_armv8.apks";
+exports.installPackagePath = "H:\\BedrockVersions\\New\\Minecraft_1.18.0b5 (1.18.0.24)_armv8.apks";
 // 安装包类型（release 表示正式版，beta 表示开发版，netease 表示网易版）
 exports.installPackageType = "beta";
 
