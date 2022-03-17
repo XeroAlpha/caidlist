124dd26da27fe262c197248ce282a58e3b1b3633
ProjectXero <projectxero@163.com>
2022-03-17 22:59:34 +0800

    feat(version): 更新至测试版1.18.30.26

--- data/config.js
+++ data/config.js
@@ -73,9 +73,9 @@ exports.packageVersions = {
     // 测试版
     beta: {
         // 安装包版本
-        version: "1.18.30.22",
+        version: "1.18.30.26",
         // 安装包路径
-        path: "H:\\BedrockVersions\\Latest\\1.18.30.22.apks",
+        path: "H:\\BedrockVersions\\Latest\\1.18.30.26.apks",
         // 可用分支
         branches: ["vanilla", "education", "experiment", "translator"],
         config: smallestGUIOCROptions
