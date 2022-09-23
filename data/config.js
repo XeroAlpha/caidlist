11ed894ec9c5dd03943fb58211870f5e5aab55d6
ProjectXero <projectxero@163.com>
2022-09-23 13:27:07 +0800

    feat(version): 更新至正式版1.19.30.04与测试版1.19.40.22

--- data/config.js
+++ data/config.js
@@ -65,9 +65,9 @@ export const packageVersions = {
     // 正式版
     release: {
         // 安装包版本
-        version: '1.19.22.01',
+        version: '1.19.30.04',
         // 安装包路径
-        path: 'H:\\BedrockVersions\\Latest\\1.19.22.01.apk',
+        path: 'H:\\BedrockVersions\\Latest\\1.19.30.04.apk',
         // 可用分支
         branches: ['vanilla', 'education', 'experiment', 'documentation'],
         config: smallestGUIOCROptions
@@ -75,9 +75,9 @@ export const packageVersions = {
     // 测试版
     beta: {
         // 安装包版本
-        version: '1.19.40.21',
+        version: '1.19.40.22',
         // 安装包路径
-        path: 'H:\\BedrockVersions\\Latest\\1.19.40.21.apk',
+        path: 'H:\\BedrockVersions\\Latest\\1.19.40.22.apk',
         // 可用分支
         branches: [
             'vanilla',
---
+++ data/gametest_behavior_pack/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true
+    },
+    extends: ["airbnb-base"],
+    parserOptions: {
+        ecmaVersion: "latest"
+    },
+    rules: {
+        indent: ["error", 4, { SwitchCase: 1 }],
+        "linebreak-style": ["error", "windows"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "comma-dangle": ["error", "never"],
+        "import/no-unresolved": ["error", { ignore: ["mojang-minecraft", "mojang-gametest"] }]
+    }
+};
