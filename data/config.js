94102a2a042f0f93b388586ca2b14379a3458faa
ProjectXero <projectxero@163.com>
2022-12-16 18:41:20 +0800

    feat(version): 更新至正式版1.19.51.01与测试版1.19.60.24

--- data/config.js
+++ data/config.js
@@ -66,9 +66,9 @@ export const packageVersions = {
     // 正式版
     release: {
         // 安装包版本
-        version: '1.19.50.02',
+        version: '1.19.51.01',
         // 安装包路径
-        path: 'H:\\BedrockVersions\\Latest\\1.19.50.02.apk',
+        path: 'H:\\BedrockVersions\\Latest\\1.19.51.01.apk',
         // 可用分支
         branches: ['vanilla', 'education', 'experiment', 'documentation'],
         config: smallestGUIOCROptions
@@ -76,9 +76,9 @@ export const packageVersions = {
     // 测试版
     beta: {
         // 安装包版本
-        version: '1.19.60.23',
+        version: '1.19.60.24',
         // 安装包路径
-        path: 'H:\\BedrockVersions\\Latest\\1.19.60.23.apk',
+        path: 'H:\\BedrockVersions\\Latest\\1.19.60.24.apk',
         // 可用分支
         branches: [
             'vanilla',
