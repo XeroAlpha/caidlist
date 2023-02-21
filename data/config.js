8927a68d91e15df5eaa68fe0d014b47566a1eaa2
ProjectXero <projectxero@163.com>
2023-02-21 20:55:04 +0800

    feat(version): 更新至中国版测试版2.5beta-218677

--- data/config.js
+++ data/config.js
@@ -94,10 +94,10 @@ export const packageVersions = {
     // 中国版测试版
     netease_dev: {
         // 安装包版本
-        version: '2.5beta-218116',
+        version: '2.5beta-218677',
         coreVersion: '1.18.30.0.0',
         // 安装包路径
-        path: 'H:\\BedrockVersions\\NeteaseDev\\dev_launcher_2.5.100.218116.apk',
+        path: 'H:\\BedrockVersions\\NeteaseDev\\dev_launcher_2.5.100.218677.apk',
         // 可用分支
         branches: ['vanilla', 'experiment'],
         config: neteaseOCROptions
