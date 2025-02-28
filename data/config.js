094739ec0d2d73cb0e77646706749b6b10711576
ProjectXero <projectxero@163.com>
2025-02-28 18:06:27 +0800

    feat(version): 更新至测试版1.21.70.25与正式版1.21.62.01

--- data/config.js
+++ data/config.js
@@ -61,9 +61,9 @@ export const packageVersions = {
     // 正式版
     release: {
         // 安装包版本
-        version: '1.21.61.01',
+        version: '1.21.62.01',
         // 安装包路径
-        path: 'H:\\BedrockVersions\\Latest\\1.21.61.01.apks',
+        path: 'H:\\BedrockVersions\\Latest\\1.21.62.01.apks',
         config: {
             ...smallestGUIOptions,
             // 仅在 Android 端暂时无法使用时使用 Windows 10 版替代
@@ -73,9 +73,9 @@ export const packageVersions = {
     // 测试版/预览版
     beta: {
         // 安装包版本
-        version: '1.21.70.24',
+        version: '1.21.70.25',
         // 安装包路径
-        path: 'H:\\BedrockVersions\\Latest\\1.21.70.24.apks',
+        path: 'H:\\BedrockVersions\\Latest\\1.21.70.25.apks',
         config: {
             ...smallestGUIOptions,
             // 仅在 Android 端暂时无法使用时使用 Windows 10 版替代
@@ -94,14 +94,14 @@ export const packageVersions = {
     // 预览版（Windows 端）
     preview_win: {
         // 应用版本
-        version: '1.21.70.24',
+        version: '1.21.70.25',
         config: {
             devBehaviorPackPath: `${process.env.LOCALAPPDATA}\\Packages\\Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe\\LocalState\\games\\com.mojang\\development_behavior_packs`
         }
     },
     dev: {
-        version: '1.21.70.24',
-        path: 'H:\\BedrockVersions\\Dev\\1.21.70.24.apk',
+        version: '1.21.70.25',
+        path: 'H:\\BedrockVersions\\Dev\\1.21.70.25.apk',
         config: smallestGUIOptions
     }
 };
