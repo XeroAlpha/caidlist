e303d8d3aa0c1679b95eecadcbc0176199f2c7fe
ProjectXero <projectxero@163.com>
2025-07-31 14:20:29 +0800

    feat(version): 更新至测试版1.21.110.22

--- data/config.js
+++ data/config.js
@@ -73,9 +73,9 @@ export const packageVersions = {
     // 测试版/预览版
     beta: {
         // 安装包版本
-        version: '1.21.110.20',
+        version: '1.21.110.22',
         // 安装包路径
-        path: 'H:\\BedrockVersions\\Latest\\1.21.110.20.apks',
+        path: 'H:\\BedrockVersions\\Latest\\1.21.110.22.apks',
         config: {
             ...smallestGUIOptions,
             // 仅在 Android 端暂时无法使用时使用 Windows 10 版替代
@@ -94,14 +94,14 @@ export const packageVersions = {
     // 预览版（Windows 端）
     preview_win: {
         // 应用版本
-        version: '1.21.110.20',
+        version: '1.21.110.22',
         config: {
             devBehaviorPackPath: `${process.env.LOCALAPPDATA}\\Packages\\Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe\\LocalState\\games\\com.mojang\\development_behavior_packs`
         }
     },
     dev: {
-        version: '1.21.110.20',
-        path: 'H:\\BedrockVersions\\Dev\\1.21.110.20.apk',
+        version: '1.21.110.22',
+        path: 'H:\\BedrockVersions\\Dev\\1.21.110.22.apk',
         config: smallestGUIOptions
     }
 };
