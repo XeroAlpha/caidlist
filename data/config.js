1e8ae9f5c8e5fa2a1aa184da823b7fd3ba938071
ProjectXero <projectxero@163.com>
2026-03-10 20:14:31 +0800

    feat(version): 更新至正式版1.26.2.1

--- data/config.js
+++ data/config.js
@@ -61,9 +61,9 @@ export const packageVersions = {
     // 正式版
     release: {
         // 安装包版本
-        version: '1.26.1.1',
+        version: '1.26.2.1',
         // 安装包路径
-        path: 'H:\\BedrockVersions\\Latest\\1.26.1.1.apks',
+        path: 'H:\\BedrockVersions\\Latest\\1.26.2.1.apks',
         config: {
             ...smallestGUIOptions,
             // 仅在 Android 端暂时无法使用时使用 Windows 10 版替代
